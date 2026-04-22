f3bbfae56f63b8eddfb54144cb2ca8429055fcbe
Kolev <kolevaz@gmail.com>
2026-04-22 06:29:10 +0300

    Add note and category service interfaces
    Introduce INoteService and ICategoryService interfaces in the Core project to define asynchronous operations for notes and categories. INoteService provides GetAllNotesAsync, GetNoteByIdAsync, AddNoteAsync, UpdateNoteAsync and DeleteNoteAsync. ICategoryService provides GetAllCategoriesAsync and AddCategoryAsync. These interfaces enable dependency inversion and provide a contract for service implementations.

---
+++ MarkdownNotesManager.Core/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkdownNotesManager.Core.Models;
+
+namespace MarkdownNotesManager.Core.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetAllCategoriesAsync();
+        Task AddCategoryAsync(Category category);
+    }
+}
---
+++ MarkdownNotesManager.Core/Interfaces/INoteService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkdownNotesManager.Core.Models;
+
+namespace MarkdownNotesManager.Core.Interfaces
+{
+    public interface INoteService
+    {
+        Task<List<Note>> GetAllNotesAsync();
+        Task<Note?> GetNoteByIdAsync(int id);
+        Task AddNoteAsync(Note note);
+        Task UpdateNoteAsync(Note note);
+        Task DeleteNoteAsync(int id);
+    }
+}
