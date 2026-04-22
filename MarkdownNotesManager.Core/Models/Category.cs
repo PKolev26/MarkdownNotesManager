6cfd33b32517f5efe83e5c2e8518991d467e0c8f
Kolev <kolevaz@gmail.com>
2026-04-22 06:09:16 +0300

    Add project files.

---
+++ MarkdownNotesManager.Core/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkdownNotesManager.Core.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<Note> Notes { get; set; } = new List<Note>();
+    }
+}
---
+++ MarkdownNotesManager.Core/Models/Note.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkdownNotesManager.Core.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+    }
+}
