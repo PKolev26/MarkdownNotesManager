1a9481985efbeed5225d52e98915e45567d2b6e4
Kolev <kolevaz@gmail.com>
2026-04-22 06:45:19 +0300

    Add MarkdownService and Markdig dependency
    Introduce MarkdownService in Infrastructure/Services to convert Markdown to HTML using the Markdig library. Update the Infrastructure csproj to add the Markdig package reference and remove the now-unused empty Services folder entry.

---
+++ MarkdownNotesManager.Infrastructure/Services/MarkdownService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Markdig;
+
+namespace MarkdownNotesManager.Infrastructure.Services
+{
+    public class MarkdownService
+    {
+        public string ToHtml(string markdown)
+        {
+            return Markdown.ToHtml(markdown ?? string.Empty);
+        }
+    }
+}
